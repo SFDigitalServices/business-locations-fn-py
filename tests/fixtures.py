7466b2fbe6c8e40658f0c3696d7614da72345ab4
Josh Chou <626661+josh-chou@users.noreply.github.com>
2022-04-20 12:59:01 -0700

    test coverage

---
+++ tests/fixtures.py
@@ -0,0 +1,19 @@
+""" Test fixtures """
+import pytest
+
+CLIENT_HEADERS = {
+    "ACCESS_KEY": "1234567"
+}
+
+@pytest.fixture
+def mock_env_access_key(monkeypatch):
+    """ mock environment access key """
+    monkeypatch.setenv("ACCESS_KEY", CLIENT_HEADERS["ACCESS_KEY"])
+    monkeypatch.setenv("ADDRESS_SVC_AUTH_API_KEY", "12345ABCDEF")
+    monkeypatch.setenv("ADDRESS_SVC_APP_TOKEN", "secret_token")
+    monkeypatch.setenv("BAN_API_URL", "https://api.url.com/ban")
+
+@pytest.fixture
+def mock_env_no_access_key(monkeypatch):
+    """ mock environment with no access key """
+    monkeypatch.delenv("ACCESS_KEY", raising=False)
---
+++ tests/mocks.py
@@ -0,0 +1,38 @@
+""" mocks for testing """
+
+SOCRATA_RESPONSE = [
+    {
+        "ban": "0000001",
+        "owners": "OWNER 1",
+        "businessname": "Business 1",
+        "dbaname": "FOO REALTY",
+        "streetaddress": "580 MAIN ST",
+        "city": "SAN FRANCISCO",
+        "state": "CA",
+        "postalcd": "11111",
+        "lin": "1111111-11-111",
+        "busstartdate": "2016-09-01T00:00:00.000",
+        "locstartdate": "2019-08-14T00:00:00.000",
+        "mailingaddress": "580 MAIN ST",
+        "mailcitystatezip": "SAN FRANCISCO CA 11111",
+        "locationnumber": "111",
+        "orgtype": "C-Corp"
+    },
+    {
+        "ban": "0000001",
+        "owners": "OWNER 2",
+        "businessname": "BUSINESS INC",
+        "dbaname": "REAL ESTATE GROUP",
+        "streetaddress": "580 MAIN ST",
+        "city": "SAN FRANCISCO",
+        "state": "CA",
+        "postalcd": "11111",
+        "lin": "1111111-11-111",
+        "busstartdate": "2016-09-01T00:00:00.000",
+        "locstartdate": "2019-08-14T00:00:00.000",
+        "mailingaddress": "580 MAIN ST",
+        "mailcitystatezip": "SAN FRANCISCO CA 11111",
+        "locationnumber": "111",
+        "orgtype": "C-Corp"
+    }
+]
